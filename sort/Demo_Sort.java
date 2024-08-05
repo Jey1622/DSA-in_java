4699f4ebbead52870ed6216c0acef4665015b8e1
Jeyaraman S <112237152+Jey1622@users.noreply.github.com>
2024-08-05 11:53:01 +0530

    ADD  Quick Sort

--- sort/Demo_Sort.java
+++ sort/Demo_Sort.java
@@ -7,6 +7,8 @@ public class Demo_Sort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] arr= {10,5,26,3,15,22};
+		int[] arr2= {4,71,12,54,25,86,12,9,34,};
+		int[] inp= {5,8,4,3,7,6,2,9,5};
 		System.out.println("--------SelectionSort---------");
 		Sort.SelectionSort_asc(arr);
 		System.out.println(Arrays.toString(arr));
@@ -20,6 +22,12 @@ public class Demo_Sort {
 		System.out.println("--------InsertionSort---------");
 		Sort.insertionSort(arr);
 		System.out.println(Arrays.toString(arr));
+		System.out.println("--------MergeSort---------");
+		arr2=mergeSort.sort(arr2);
+		System.out.println(Arrays.toString(arr2));
+		System.out.println("--------QuickSort---------");
+		Sort.quickSort(inp, 0, inp.length-1);
+		System.out.println(Arrays.toString(inp));
 	}
 
 }
