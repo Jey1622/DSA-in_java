157045ea566e70a158abdf3325da1fa471a209fc
Jeyaraman S <112237152+Jey1622@users.noreply.github.com>
2024-08-06 12:08:42 +0530

    change directory

---
+++ Tree/BinarySearchTree/Demo_BST.java
@@ -0,0 +1,24 @@
+package dsa.java;
+
+public class Demo_BST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinarySearchTree bst=new BinarySearchTree(50);
+		bst.insert(20);
+		bst.insert(70);
+		bst.insert(10);
+		bst.insert(25);
+		bst.insert(60);
+		bst.insert(90);
+		bst.inOrder(bst.root);
+		if(bst.Search(bst.root, 70)==null)
+			System.out.println("Not found");
+		else 
+			System.out.println("found");
+		bst.delete(70);
+		bst.inOrder(bst.root);
+		System.out.println();
+	}
+
+}
